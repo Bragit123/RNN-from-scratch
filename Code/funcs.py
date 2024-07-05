d19a24984646fe2952fc07b75e2074103f21fcb3
Brage Andreas Trefjord <brageandreastrefjord@Brage-sin-Air.lan>
2024-07-05 13:16:11 +0200

    Added funcs.py and schedulers.py from FYS5429 lecture notes.

---
+++ Code/funcs.py
@@ -0,0 +1,81 @@
+
+import jax.numpy as jnp
+import numpy as np
+from jax import grad
+
+def CostOLS(target):
+
+    def func(X):
+        return (1.0 / target.shape[0]) * jnp.sum((target - X) ** 2)
+
+    return func
+
+
+def CostLogReg(target):
+
+    def func(X):
+
+        return -(1.0 / target.shape[0]) * jnp.sum(
+            (target * jnp.log(X + 10e-10)) + ((1 - target) * jnp.log(1 - X + 10e-10))
+        )
+
+    return func
+
+
+def CostCrossEntropy(target):
+
+    def func(X):
+        return -(1.0 / target.size) * jnp.sum(target * jnp.log(X + 10e-10))
+
+    return func
+
+
+def identity(X):
+    return X
+
+
+def sigmoid(X):
+    try:
+        return 1.0 / (1 + jnp.exp(-X))
+    except FloatingPointError:
+        return jnp.where(X > jnp.zeros(X.shape), jnp.ones(X.shape), jnp.zeros(X.shape))
+
+
+def softmax(X):
+    X = X - np.max(X, axis=-1, keepdims=True)
+    delta = 10e-10
+    return np.exp(X) / (np.sum(np.exp(X), axis=-1, keepdims=True) + delta)
+
+
+def grad_softmax(X):
+    f = softmax(X)
+    return f - f**2
+    
+
+def RELU(X):
+    return jnp.where(X > jnp.zeros(X.shape), X, jnp.zeros(X.shape))
+
+
+def LRELU(X):
+    delta = 10e-4
+    return jnp.where(X > jnp.zeros(X.shape), X, delta * X)
+
+
+def derivate(func):
+    if func.__name__ == "RELU":
+
+        def func(X):
+            return jnp.where(X > 0, 1, 0)
+
+        return func
+
+    elif func.__name__ == "LRELU":
+
+        def func(X):
+            delta = 10e-4
+            return jnp.where(X > 0, 1, delta)
+
+        return func
+
+    else:
+        return grad(func)
