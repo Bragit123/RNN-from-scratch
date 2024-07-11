00788e3c866ddb887ad3e27c0bb1214dad6da842
Brage Andreas Trefjord <b.a.trefjord@fys.uio.no>
2024-07-11 15:38:24 +0200

    Started implementing train() in RNN. Will continue this next time

--- Code/RNN.py
+++ Code/RNN.py
@@ -1,4 +1,7 @@
 from collections.abc import Callable
+from jax import vmap
+from funcs import derivate
+from sklearn.utils import resample
 from schedulers import Scheduler
 from Layer import Layer
 from InputLayer import InputLayer
@@ -10,11 +13,23 @@ import numpy as np
 class RNN:
     def __init__(
             self,
+            cost_func: Callable[
+                [np.ndarray], # Takes in the target output (array)
+                Callable[[np.ndarray], np.ndarray] # Returns a function (the cost function)
+            ],
             seed: int = 100
     ):
-        self.layers: list[Layer] = []
+        """
+        cost_func = function which takes in the target array and returns a new function. The new function
+            takes in the output from the network, and returns the cost of that output compared to the target.
+        """
+        self.layers = [] # List of layers
         self.n_layers = 0
+
+        self.cost_func = cost_func
         self.seed = seed
+
+        self.n_features_output = None
     
     def reset_weights(self):
         for layer in self.layers:
@@ -24,22 +39,86 @@ class RNN:
             self,
             X: np.ndarray
     ):
+        """
+        X = input to RNN.
+        X_shape = (batch_size, sequence_length, n_features)
+        NOTE: Unlike the other layers, this layer takes a numpy array as input instead of a Layer.
+        """
+        X_shape = X.shape
+
+        ## Initialize output
+        n_batches = X_shape[0]
+        sequence_length = X_shape[1]
+        n_features_output = self.n_features_output
+        output_shape = (n_batches, sequence_length, n_features_output)
+        output = np.zeros(output_shape)
+
         self.layers[0].feed_forward(X)
         for i in range(1, self.n_layers):
             layer = self.layers[i]
             prev_layer = self.layers[i-1]
             layer.feed_forward(prev_layer)
+        
+        output_layer = layer
+        for i in range(output_layer.n_nodes):
+            node = output_layer.nodes[i]
+            output[:,i,:] = node.h_output
+        
+        return output
+    
+    def backpropagate(
+            self,
+            output: np.ndarray,
+            target: np.ndarray,
+            lmbd: float = 0.01
+    ):
+        grad_cost = derivate(self.cost_func(target))
+        dC = grad_cost(output)
+        self.layers[-1].backpropagate(dC, lmbd)
 
+        for i in range(self.n_layers-2, 0, -1):
+            layer = self.layers[i]
+            next_layer = self.layers[i+1]
+            layer.backpropagate(next_layer, lmbd)
+        
     def train(
             self,
             X_train: np.ndarray,
-            y_train: np.ndarray,
-            X_test: np.ndarray,
-            y_test: np.ndarray,
-            lmbd: float,
-            epochs: int
+            t_train: np.ndarray,
+            X_val: np.ndarray = None,
+            t_val: np.ndarray = None,
+            epochs: int = 100,
+            batches: int = 1,
+            lmbd: float = 0.01
     ):
-        pass
+        """
+        X = input
+        t = target
+        _train = training data
+        _val = validation data
+        epochs = number of epochs to train for
+        batches = number of batches to split data into
+        """
+        self.reset_weights() # Reset weights for new training
+        batch_size = X_train.shape[0] // batches
+
+        # Initialize arrays for training scores
+        train_cost = self.cost_func(t_train)
+        train_error = np.zeros(epochs)
+        train_accuracy = np.zeros(epochs)
+
+        if X_val is not None:
+            val_cost = self.cost_func(t_val)
+            val_error = np.zeros(epochs)
+            val_accuracy = np.zeros(epochs)
+        
+        # Resample X and t
+        X_train, t_train = resample(X_train, t_train, replace=False)
+
+        for e in range(epochs):
+            print("EPOCH: " + str(e+1) + "/" + str(epochs))
+
+
 
     def add_InputLayer(
             self,
@@ -84,6 +163,8 @@ class RNN:
         n_features_prev = prev_layer.n_features
         layer = OutputLayer(n_features, n_features_prev, act_func, scheduler, self.seed)
         self._add_layer(layer)
+        
+        self.n_features_output = n_features
     
     def _add_layer(
             self,
--- Code/test.py
+++ Code/test.py
@@ -1,5 +1,5 @@
 import numpy as np
-from funcs import sigmoid, identity
+from funcs import sigmoid, identity, CostOLS
 from RNN import RNN
 from schedulers import Constant, Adam
 from InputLayer import InputLayer
@@ -7,13 +7,13 @@ from RNNLayer import RNNLayer
 from OutputLayer import OutputLayer
 from Node import Node
 
-print("##### FEED FORWARD #####")
+### Feed forward
 
+cost_func = CostOLS
 act_func = sigmoid
-# scheduler = Constant(eta=0.1)
 scheduler = Adam(eta=0.1, rho=0.9, rho2=0.999)
 
-rnn = RNN()
+rnn = RNN(cost_func)
 rnn.add_InputLayer(3)
 rnn.add_RNNLayer(2, act_func, scheduler)
 rnn.add_OutputLayer(3, act_func, scheduler)
@@ -21,47 +21,15 @@ rnn.reset_weights()
 
 np.random.seed(100)
 X = np.random.uniform(size=(5,4,3))
-rnn.feed_forward(X)
+output = rnn.feed_forward(X)
 
-for i in range(rnn.n_layers):
-    layer = rnn.layers[i]
-    print("W_layer:")
-    print(layer.W_layer)
-    print()
-    print("b_layer:")
-    print(layer.b_layer)
-    print()
-    print("W_time:")
-    print(layer.W_time)
-    print()
-    print("b_time:")
-    print(layer.b_time)
-    print()
-    for j in range(layer.n_nodes):
-        node = layer.nodes[j]
-        print(f"({i},{j}):")
-        print(node.get_output())
+### Backpropagation
 
+output_shape = output.shape
+target = np.random.uniform(size=output_shape)
 
-print()
-print()
-print()
-print("##### BACKPROPAGATION #####")
-
-last_layer = rnn.layers[-1]
-hidden_layer = rnn.layers[1]
-
-dC = np.random.uniform(size=(5, last_layer.n_nodes, 3))
-last_layer.backpropagate(dC, lmbd=0.01)
-hidden_layer.backpropagate(last_layer, lmbd=0.01)
-
-for i in range(hidden_layer.n_nodes):
-    node = hidden_layer.nodes[i]
-    print(f"i = {i}")
-    print(f"grad_b_layer = {node.grad_b_layer}")
-    print(f"grad_b_time = {node.grad_b_time}")
-    print(f"grad_W_layer = {node.grad_W_layer}")
-    print(f"grad_W_time = {node.grad_W_time}")
-    print(f"grad_h_layer = {node.grad_h_layer}")
-    print(f"grad_h_time = {node.grad_h_time}")
-    print()
+print(cost_func(target)(output))
+for i in range(100):
+    rnn.backpropagate(output, target, lmbd=0.01)
+    output = rnn.feed_forward(X)
+    print(cost_func(target)(output))
