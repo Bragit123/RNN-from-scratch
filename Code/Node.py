Minor fix. Hard to test backprop results, but they look fine. Moving on now

Code/Node.py
@@ -95,7 +95,8 @@ class Node:
     def backpropagate(
             self,
             dC_layer: np.ndarray,
-            dC_time: np.ndarray = None
+            dC_time: np.ndarray = None,
+            lmbd: float = 0.01
     ):
         """
         dC_layer/dC_time = Contribution of cost gradient w.r.t. this node from node at next layer/time
@@ -112,19 +113,18 @@ class Node:
         grad_act = vmap(vmap(derivate(self.act_func)))(self.z_output) # vmap is necessary for jax to vectorize gradient properly
         delta = grad_act * dC # Hadamard product, i.e., elementwise multiplication
 
+
         ## Gradients w.r.t. bias
-        self.grad_b_layer = delta
-        self.grad_b_time = delta
+        self.grad_b_layer = np.sum(delta, axis=0)
+        self.grad_b_time = np.sum(delta, axis=0)
 
         ## Gradients w.r.t. weights
-        print(self.h_layer)
-        print(delta)
         # Need to transpose h and not delta in order for matrices to match up correctly, since we have batches along rows, and features along columns
         self.grad_W_layer = self.h_layer.T @ delta
         self.grad_W_time = self.h_time.T @ delta
 
         ## Gradients w.r.t. input from previous nodes
-        # Need to transpose not transpose delta in order for matrices to match up correctly, since we have batches along rows, and features along columns
-        self.grad_h_layer = delta @ self.W_layer.T
-        self.grad_h_time = delta @ self.W_time.T
+        # Need to not transpose delta in order for matrices to match up correctly, since we have batches along rows, and features along columns
+        self.grad_h_layer = np.sum(delta @ self.W_layer.T, axis=0)
+        self.grad_h_time = np.sum(delta @ self.W_time.T, axis=0)
 